75634c0b465b9e3f72dc6cf71c1062a3357ea69d
Parida <parida.debasis@dorseylaw.corp>
2017-05-12 14:19:14 -0500

    file path changes

---
+++ dev/contacts/mockContact.ts
@@ -0,0 +1,7 @@
+import { contact } from './contact';
+export const CONTACTS: contact[] = [
+    {firstName: "Debasis", lastName: "Parida", phone: "1111111111",email: "debasis@mail.com" },
+    { firstName: "Debasis2", lastName: "Parida2", phone: "2222222222",email: "debasis2@mail.com" },
+    { firstName: "Debasis3", lastName: "Parida3", phone: "3333333333",email: "debasis3@mail.com" }
+
+];
