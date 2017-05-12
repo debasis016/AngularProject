575bac59908c0431ce6ec3bfea1b5f2721e504cc
Parida <parida.debasis@dorseylaw.corp>
2017-05-12 17:00:51 -0500

    remove template from appcomponent

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,31 +1,14 @@
 import { Component } from 'angular2/core';
+import { ContactListComponent } from "./contacts/contactList.component";
 
 @Component({
     selector: 'my-app',
     template: `
-    <ul>
-    <li *ngFor = "#contact of contacts"
-        (click)="onSelect(contact)"
-        [class.clicked]="showDetail === true">
-        {{contact.firstName}} {{contact.lastName}}
-    </li>
-    <input [(ngModel)] = "selectedContact.firstName" type="text">
-        <div>
-            First Name: {{selectedContact.firstName}}<br>
-            Last Name:  {{selectedContact.lastName}}
-        </div>
-    </ul>
-        
+    <contact-List></contact-List>
     `,
-    styleUrls: ["../src/css/app.css"]
+    directives:[ContactListComponent]
+    
 })
 export class AppComponent {
-    public contacts = [{ firstName: "Debasis", lastName: "Parida", phone: "1111111111" },
-    { firstName: "Debasis2", lastName: "Parida2", phone: "2222222222" },
-    { firstName: "Debasis3", lastName: "Parida3", phone: "3333333333" }];
-    public showDetail = false;
-    public selectedContact = {};
-    onSelect(contact) {
-        this.selectedContact = contact;
-    }
+    
 }
